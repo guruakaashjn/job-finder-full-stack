1314a416b496cc4a70073266889b9ac4c687cd79
guruakaashjn <gurujnn@gmail.com>
2025-02-18 19:35:27 +0530

    update error message

--- server/middleware/protect.js
+++ server/middleware/protect.js
@@ -2,7 +2,7 @@ const protect = (req, res, next) => {
     if (req.oidc.isAuthenticated()) {
         next();
     } else {
-        res.status(401).json({ message: "Noth Authorized" });
+        res.status(401).json({ message: "Not Authorized" });
     }
 }
 
