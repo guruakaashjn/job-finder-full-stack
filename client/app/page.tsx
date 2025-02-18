badfe1dffe473afbe0508a32a611400710e72424
guruakaashjn <gurujnn@gmail.com>
2025-02-18 19:37:13 +0530

    add shadcn dependency library

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,101 +1,164 @@
-import Image from "next/image";
+"use client";
+
+import Link from "next/link";
+import { useGlobalContext } from "@/context/globalContext";
+import Header from "@/Components/Header";
+import Footer from "@/Components/Footer";
+import { Input } from "@/Components/ui/input";
+import { Button } from "@/Components/ui/button";
+import {
+  Briefcase,
+  Building,
+  SearchIcon,
+  Users,
+  CheckCircleIcon,
+} from "lucide-react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/Components/ui/card";
+import { Badge } from "@/Components/ui/badge";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { isAuthenticated } = useGlobalContext();
+  console.log(isAuthenticated);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const features = [
+    {
+      icon: <Briefcase className="w-6 h-6 text-[#7263f3]" />,
+      title: "Diverse Opportunities",
+      description:
+        "Access thousands of job listings across various industries and experience levels.",
+      benefits: [
+        "100,000+ active job listings",
+        "50+ job categories",
+        "Remote and on-site options",
+      ],
+      cta: "Explore Jobs",
+      ctaLink: "/findwork",
+    },
+    {
+      icon: <Building className="w-6 h-6 text-[#7263f3]" />,
+      title: "Top Companies",
+      description:
+        "Connect with leading companies, from innovative startups to Fortune 500 corporate companies.",
+      benefits: [
+        "500+ verifies employers",
+        "Exclusive partnerships",
+        "Direct application process",
+      ],
+      cta: "View Companies",
+      ctaLink: "/findwork",
+    },
+    {
+      icon: <Users className="w-6 h-6 text-[#7263f3]" />,
+      title: "Talent Pool",
+      description:
+        "Employers can access a diverse pool of qualified candidates for their open positions",
+      benefits: [
+        "1M+ registered job seekers",
+        "Advanced search filters",
+        "AI-powered matching",
+      ],
+      cta: "Post a Job",
+      ctaLink: "/post",
+    },
+  ];
+
+  return (
+    <main>
+      <Header />
+      <section className="py-20 bg-gradient-to-b from-[#d7dedc] to-[#7263f3]/5 text-primary-foreground">
+        <div className="container mx-auto px-4 text-center text-black">
+          <h1 className="text-4xl text-[#7263f3] md:text-5xl font-bold mb-6">
+            Find Your Dream Job or a Perfect Candidate
+          </h1>
+          <p className="text-xl mb-8">
+            Connect with millions or employers and job seekers on our platform
+          </p>
+          <div className="max-w-2xl m-auto flex gap-4">
+            <Input
+              type="text"
+              placeholder="Search by title or keyword"
+              className="flex-grow bg-white text-black"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <Button className="bg-[#7263f3] text-white">
+              <SearchIcon className="w-6 h-6" />
+              Search Jobs
+            </Button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+
+      <section className="py-20 bg-[#f0f5fa]">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-12">
+            Why Choose{" "}
+            <span className="font-extrabold text-[#7263f3]">JobSphere</span>
+          </h2>
+
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {features.map((feature, index) => (
+              <Card
+                key={index}
+                className="flex flex-col h-full rounded-xl border-none"
+              >
+                <CardHeader>
+                  <div className="w-12 h-12 rounded-full bg-primary/10 flex items-center justify-center mb-4">
+                    {feature.icon}
+                  </div>
+                  <CardTitle className="text-xl mb-2">
+                    {feature.title}
+                  </CardTitle>
+                  <CardDescription>{feature.description}</CardDescription>
+                </CardHeader>
+                <CardContent className="flex-grow">
+                  <ul className="space-y-2">
+                    {feature.benefits.map((benefit, index) => (
+                      <li key={index} className="flex items-center">
+                        <CheckCircleIcon className="w-5 h-5 text-green-500 mr-2 flex-shrink-0 mt-0.5" />
+                        <span>{benefit}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </CardContent>
+                <CardFooter>
+                  <Button asChild className="w-full">
+                    <Link href={feature.ctaLink}>{feature.cta}</Link>
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+          <div className="mt-12 text-center">
+            <Badge
+              variant={"outline"}
+              className="text-sm font-medium border-gray-400"
+            >
+              Trusted by 10,000+ companies worldwide
+            </Badge>
+          </div>
+        </div>
+      </section>
+      <section className="py-[7rem] bg-[#d7dedc]">
+        <div className="container mx-auto px-4 text-center">
+          <h2 className="text-3xl font-bold mb-8">Ready to Get Started?</h2>
+          <div className="flex flex-col md:flex-row justify-center gap-4">
+            <Button size={"lg"} asChild>
+              <Link href={"/findwork"}>Find Work</Link>
+            </Button>
+            <Button size={"lg"} variant={"outline"} asChild>
+              <Link href={"/post"}>Post a Job</Link>
+            </Button>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </main>
   );
 }
